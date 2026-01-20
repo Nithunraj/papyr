1e5428f111beb8b32f69faaa79ae4488b9d8d968
Nithunraj <nithunrajnm@gmail.com>
2026-01-20 17:29:40 +0530

    allowed host changes

--- paypr/settings.py
+++ paypr/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = 'django-insecure-u8*0wq5tg-ib_*sggdf5+fx^eg!5&+7)#7s#=$800k!tg&o*js
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['papyr-us0e.onrender.com']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['papyr-us0e.onrender.com']
+# ALLOWED_HOSTS = []
 
 
 AUTH_USER_MODEL = 'accounts.User'
