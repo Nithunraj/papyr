118543bd147ab8c3cc564648e394e6046b8aa49c
Nithunraj <nithunrajnm@gmail.com>
2026-01-22 10:31:23 +0530

    updated using classes to close and cancel buttons js

--- accounts/static/accounts/js/profile.js
+++ accounts/static/accounts/js/profile.js
@@ -1,32 +1,33 @@
 const editBtn = document.getElementById("editProfileBtn");
 const modal = document.getElementById("editProfileModal");
-const closeBtn = document.getElementById("close-modal-btn");
-const profileCancelButton = document.getElementById("closeModal");
+
+const passwordChangeBtn = document.getElementById("changePasswword");
+const changePasswordModal = document.getElementById("changePasswordButton");
+
+const closeButtons = document.querySelectorAll(".close-modal");
+const modalOverlays = document.querySelectorAll(".modal-overlay");
 
 editBtn.addEventListener("click", function (e) {
   e.preventDefault();
   modal.classList.add("active");
 });
 
-closeBtn.addEventListener("click", function () {
-  modal.classList.remove("active");
-});
-
-profileCancelButton.addEventListener("click", function () {
-  modal.classList.remove("active");
-});
-
-// Close when clicking outside modal
-modal.addEventListener("click", function (e) {
-  if (e.target === modal) {
-    modal.classList.remove("active");
-  }
-});
-
-const passwordChangeBtn = document.getElementById("changePasswword");
-const changePasswordModal = document.getElementById("changePasswordButton");
-
 passwordChangeBtn.addEventListener("click", function (e) {
   e.preventDefault();
   changePasswordModal.classList.add("active");
+});
+
+closeButtons.forEach(btn => {
+  btn.addEventListener("click", () => {
+    modalOverlays.forEach(modal => modal.classList.remove("active"));
+  });
+});
+
+// Close when clicking outside modal
+modalOverlays.forEach(modal => {
+  modal.addEventListener("click", e => {
+    if (e.target === modal) {
+      modal.classList.remove("active");
+    }
+  });
 });
